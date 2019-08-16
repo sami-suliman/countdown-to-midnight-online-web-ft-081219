1493d0218f93d97cb44579a56d70af2b286c6fb4
sami suliman <sami.bco@gmail.com>
2019-08-16 03:24:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,12 +11,11 @@ end
 countdown(10)
 
 def countdown_with_sleep(x)
-   
   while x > 0 
-  puts "#{x} SECOND(S)!"
-  sleep 1
-   x -=1 
-end 
- return "HAPPY NEW YEAR!"
+    puts "#{x} SECOND(S)!"
+    sleep 1
+    x -=1 
+  end 
+return "HAPPY NEW YEAR!"
 end
 countdown_with_sleep(10)
